d5f36d9bd8f1c557d1b6435ea1a37a8a9bf9f888
nathan ganesh <489290@student.saxion.nl>
2020-11-16 20:37:47 +0100

    react router working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import './index.css';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import News from './pages/News';
+import Videos from './pages/Videos';
+import Error from './pages/Error';
+import VideoDetail from './pages/VideoDetail';
+import Article from './pages/Article';
+
 function App() {
-	return <div className="App">hey</div>;
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Footer />
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/news" component={News} />
+				<Route exact path="/videos" component={Videos} />
+				<Route exact path="/videos/:id" component={VideoDetail} />
+				<Route exact path="/articles/:id" component={Article} />
+				<Route component={Error} />
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
