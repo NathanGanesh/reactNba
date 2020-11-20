39e77faec243c01e4bcb4cd92ac6d35f96044ecc
nathan ganesh <489290@student.saxion.nl>
2020-11-20 18:25:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './index.css';
 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import News from './pages/News';
-import Videos from './pages/Videos';
-import Error from './pages/Error';
-import VideoDetail from './pages/VideoDetail';
-import Article from './pages/Article';
-import Submenu from './components/Submenu';
-
 function App() {
 	return (
 		<BrowserRouter>
-			<Navbar />
-			<Submenu />
-			<Switch>
-				<Route exact path="/" component={Home} />
-				<Route exact path="/news" component={News} />
-				<Route exact path="/videos" component={Videos} />
-				<Route exact path="/videos/:id" component={VideoDetail} />
-				<Route exact path="/articles/:id" component={Article} />
-				<Footer />
-				<Route component={Error} />
-			</Switch>
+			<Switch />
 		</BrowserRouter>
 	);
 }
--- src/components/VideoItem.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function VideoItem() {
-	return <div>VideoItem</div>;
-}
